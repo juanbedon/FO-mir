37b63806a0784301c1cb328b6bafaacc4d8c394b
Juan <juanbedon26@hotmail.com>
2018-07-15 15:00:26 -0500

    Create questions functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,22 @@ class QuestionsController < ApplicationController
 		@question = Question.new
 	end
 
+	def create
+
+		@question = Question.new(question_params)
+		
+		if @question.save
+			redirect_to questions_path, notice: 'Your question was submitted successfully!'
+		else
+			render :new
+		end
+
+	end
+
+	private
+
+		def question_params
+			params.require(:question).permit(:title, :description)
+		end
+
 end
