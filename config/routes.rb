e0dfa5797626494df3bb2830d8e994ca2a39f7b5
Juan <juanbedon26@hotmail.com>
2018-07-15 15:31:19 -0500

    Created show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	get '/questions', to: 'questions#index'
 	get '/questions/new', to: 'questions#new', as: 'new_question'
 	post '/questions', to: 'questions#create'
+	get '/questions/:id', to: 'questions#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
