2e655a84fa710402879608a72d05ee86a313f8fd
Juan <juanbedon26@hotmail.com>
2018-07-15 13:28:32 -0500

    Created Question model and its migration

---
+++ db/migrate/20180715132100_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+
+	def change
+		create_table :questions do |t|
+			t.string :title
+			t.text :description
+
+			t.timestamps null: false
+		end
+	end
+
+end
