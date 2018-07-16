7797b94234e3a365aba58905df913a64b8df8884
Juan <juanbedon26@hotmail.com>
2018-07-15 19:07:03 -0500

    Added link to edit questions in index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get '/questions/new', to: 'questions#new', as: 'new_question'
 	post '/questions', to: 'questions#create'
 	get '/questions/:id', to: 'questions#show', as: 'question'
-	get '/questions/:id/edit', to: 'questions#edit'
+	get '/questions/:id/edit', to: 'questions#edit', as: 'edit_question'
 	patch '/questions/:id', to: 'questions#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
