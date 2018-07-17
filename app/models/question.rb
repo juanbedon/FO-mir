d5d7cd4d6ff04e08e81540a31081f146b49772f5
Juan <juanbedon26@hotmail.com>
2018-07-17 11:20:23 -0500

    Added Annotate gem

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Question < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string(100)
+#  password_digest :string
+#  name            :string(100)
+#  username        :string(50)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
 	has_secure_password validations: false
 end
